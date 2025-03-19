aad7a0a95c15905845546fde0892afacc7ef849a
Ethan-Paes-27 <epaes27@jesuitmail.org>
2025-03-19 10:00:14 -0700

    biginteger
    trying to get it to work with big integers

--- FactorProductOfPrimes.java
+++ FactorProductOfPrimes.java
@@ -7,57 +7,57 @@ public class FactorProductOfPrimes { //https://www.youtube.com/watch?v=-UrdExQW0
     int countR;
 
     /**
-     * 
+     *
      * @param n, which is the product of two prime numbers
      * @return Point, which is the two factors of n in the order smaller number, bigger number
      */
-    public void factorProductOfPrimes(int n) {
+    public void factorProductOfPrimes(long n) {
         System.out.println("Calculating...");
 
         // if it is divisible by 2, return 2 and n / 2
         if (n % 2 == 0) {
-            System.out.println(factorPrinter(n, 2, n / 2));
+            System.out.println(factorPrinter(n, BigInteger.TWO, BigInteger.valueOf(n / 2)));
         }
 
         // if it has a perfect square root, return its square roots
         double d1 = Math.sqrt(n);
         if (d1 == Math.floor(d1) && d1 == Math.ceil(d1)) {
-                System.out.println(factorPrinter(n, (int)Math.sqrt(n), (int)Math.sqrt(n)));
+            System.out.println(factorPrinter(n, BigInteger.valueOf((long) Math.sqrt(n)), BigInteger.valueOf((long) Math.sqrt(n))));
         }
-        
+
         USED_INTEGERS.add(1);
 
-        int gPowRPlus1 = findGPowRPlus1(n);
-        int firstNum = getFirstNum(gPowRPlus1, n);
-        
+        BigInteger gPowRPlus1 = findGPowRPlus1(n);
+        BigInteger firstNum = getFirstNum(gPowRPlus1, n);
+
         //if firstNum is one, keeps finding a new one
-        while (Math.abs(firstNum) == 1 || Math.abs(firstNum) == n) {
+        while (firstNum.abs().equals(BigInteger.ONE) || firstNum.abs().equals(BigInteger.valueOf(n))) {
             gPowRPlus1 = findGPowRPlus1(n);
             firstNum = getFirstNum(gPowRPlus1, n);
         }
 
-        int secondNum = n / firstNum;
+        BigInteger secondNum = BigInteger.valueOf(n).divide(firstNum);
 
         System.out.println(factorPrinter(n, firstNum, secondNum));
     }
 
     /**
-     * 
+     *
      * @param n, the product of two prime numbers
      * @param firstNum, calculated using findFirstNum
      * @param secondNum, calculated using n / firstNum
      * @return A string converter to print the factors of n
      */
-    private String factorPrinter(int n, int firstNum, int secondNum) {
-        return "The factors of " + n + " are " + Math.min(firstNum, secondNum) + " and " + Math.max(firstNum, secondNum) + ".";
+    private String factorPrinter(long n, BigInteger firstNum, BigInteger secondNum) {
+        return "The factors of " + n + " are " + firstNum.min(secondNum) + " and " + firstNum.max(secondNum) + ".";
     }
 
     /**
-     * 
+     *
      * @param n, which is the product of two prime numbers
      * @return GPowRPlus1, which shares a common multiple with n
      */
-    private int findGPowRPlus1(int n) {
+    private BigInteger findGPowRPlus1(long n) {
         int g = findCoprime(n);
         int r = findR(n, g);
 
@@ -70,20 +70,20 @@ public class FactorProductOfPrimes { //https://www.youtube.com/watch?v=-UrdExQW0
             foundR = r % 2 == 0 || r == -1;
         }
 
-        BigInteger gPowRPlus1 = BigInteger.valueOf((long)((int)Math.pow(g, r/2) + 1));
+        BigInteger gPowRPlus1 = BigInteger.valueOf((long)Math.pow(g, (double) r /2) + 1);
 
         if (!isGPowRPlus1Okay(gPowRPlus1, n)) { //Checks to make sure that GPowRPlus1 is okay, otherwise uses recursion
             return findGPowRPlus1(n);
         }
-        else return gPowRPlus1.intValue();
+        else return gPowRPlus1;
     }
 
     /**
-     * 
+     *
      * @param n, which is the product of two prime numbers
      * @return g, a coprime of n (has no common factors with n)
      */
-    private int findCoprime(int n) {
+    private int findCoprime(long n) {
         int g = 0;
         boolean foundG = false;
 
@@ -100,11 +100,11 @@ public class FactorProductOfPrimes { //https://www.youtube.com/watch?v=-UrdExQW0
     }
 
     /**
-     * 
+     *
      * @param n, which is the product of two prime numbers
      * @return e, a random even number that is less than n
      */
-    private int randomEvenNumber(int n) {
+    private int randomEvenNumber(long n) {
         int e = 1;
         while (e % 2 == 1 || e >= n) {
             e = (int)(Math.random() * n) + 1;
@@ -114,12 +114,12 @@ public class FactorProductOfPrimes { //https://www.youtube.com/watch?v=-UrdExQW0
     }
 
     /**
-     * 
+     *
      * @param n, which is the product of two prime numbers
      * @param g, which is a coprime of n
      * @return r, which is some power where g^r modded by n is 1
      */
-    private int findR(int n, int g) {
+    private int findR(long n, int g) {
         boolean foundR = false;
         int r = 1;
         int countR = 0;
@@ -142,37 +142,37 @@ public class FactorProductOfPrimes { //https://www.youtube.com/watch?v=-UrdExQW0
     }
 
     /**
-     * 
+     *
      * @param gPowRPlus1, gPowRPlus1 from above
      * @param n, which is the product of two prime numbers
      * @return a boolean, which tells if the remainder after modding gPowRPlus1 by n is NOT 0 (false = 0)
      */
-    private boolean isGPowRPlus1Okay(BigInteger gPowRPlus1, int n) {
-        BigInteger nBig = BigInteger.valueOf((long)n);
+    private boolean isGPowRPlus1Okay(BigInteger gPowRPlus1, long n) {
+        BigInteger nBig = BigInteger.valueOf(n);
         return gPowRPlus1.mod(nBig).intValue() != 0;
     }
 
     /**
-     * 
+     *
      * @param gPowRPlus1, which is g^(r/2) + 1 (found above)
      * @param n, which is the product of two prime numbers
      * @return theFirstNum, which uses Euclid's Algorithm to get a common factor of gPowRPlus1 and n (is technically the remainder or bottom)
      */
-    private int getFirstNum(int gPowRPlus1, int n) {
+    private BigInteger getFirstNum(BigInteger gPowRPlus1, long n) {
         boolean foundR = false;
 
-        int top = Math.max(gPowRPlus1, n);
-        int bottom = Math.min(gPowRPlus1, n);
-        int remainder = top % bottom;
+        BigInteger top = gPowRPlus1.max(BigInteger.valueOf(n));
+        BigInteger bottom = gPowRPlus1.min(BigInteger.valueOf(n));
+        BigInteger remainder = top.abs().mod(bottom.abs());
 
-        if (remainder == 0) {
+        if (remainder.equals(BigInteger.ZERO)) {
             return bottom;
         }
 
         while (!foundR) {
-            int tempRem = remainder;
-            if (bottom % remainder != 0) {
-                remainder = bottom % remainder;
+            BigInteger tempRem = remainder;
+            if (!bottom.mod(remainder).equals(BigInteger.ZERO)) {
+                remainder = bottom.mod(remainder);
                 bottom = tempRem;
             } else foundR = true;
         }
@@ -182,6 +182,6 @@ public class FactorProductOfPrimes { //https://www.youtube.com/watch?v=-UrdExQW0
 
     public static void main(String[] args) {
         FactorProductOfPrimes f = new FactorProductOfPrimes();
-        f.factorProductOfPrimes(599 * 601);
+        f.factorProductOfPrimes(3917 * 2851);
     }
 }
